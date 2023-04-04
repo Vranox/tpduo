067c47b9094f842bc6b3588bd0cb7edb4bc379ed
Vranox <damyscus@gmail.com>
2023-04-04 16:52:52 +0200

    update

--- app/src/main/java/com/example/tpnote/MainActivity.java
+++ app/src/main/java/com/example/tpnote/MainActivity.java
@@ -3,6 +3,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,10 +11,13 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements edu.frallo.myapplication.PostExecuteActivity<Pokemon> {
     public static String language = "english";
+    List<Pokemon> itemList = new ArrayList<>();
     private final String TAG = "frallo "+getClass().getSimpleName();
     @SuppressLint("MissingInflatedId")
     @Override
@@ -40,12 +44,17 @@ public class MainActivity extends AppCompatActivity implements edu.frallo.myappl
         findViewById(R.id.go).setOnClickListener( clic -> {
             String url = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/17d33dc111ffcc12b016d6485152aa3b1939c214/pokedex.json";
             new HttpAsyncGet<>(url, Pokemon.class, this, new ProgressDialog(clic.getContext()) );
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            intent.putExtra("pokemonList", (Serializable) itemList);
+            intent.putExtra("language", language);
+            startActivity(intent);
         });
     }
 
 
     @Override
     public void onPostExecutePokemons(List<Pokemon> itemList) {
+        this.itemList = itemList;
         Pokemon pokemonFirst = itemList.get(0);
         Log.d(TAG,"First pokemon = " + pokemonFirst.toString());
         Log.d(TAG, language);
