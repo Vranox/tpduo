21ce6272c150871f74aa98ebba6a5b0df42e0e9f
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-04-04 16:44:17 +0200

    2eme page

---
+++ app/src/main/java/com/example/tpnote/ListPokemonFragment.java
@@ -0,0 +1,44 @@
+package com.example.tpnote;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListPokemonFragment extends Fragment {
+    private final String TAG = "Simon&Damien "+getClass().getSimpleName();
+
+    public ListPokemonFragment() {
+        Log.d(TAG, "FragmentA created");
+    }
+
+
+    @Override
+    @SuppressLint("MissingInflatedId")
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_pokemon_fragment, container, false);
+        // Obtenez une référence à la ListView
+        ListView listView = (ListView) view.findViewById(R.id.list_view);
+
+
+        List<String> dataList = new ArrayList<>();
+        dataList.add("Element 1");
+        dataList.add("Element 2");
+        dataList.add("Element 3");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, dataList);
+
+        listView.setAdapter(adapter);
+
+        return view; }
+}
